909d20410c1b36fc0db8642a9c16401a13ed7c02
SabrinaPerdiz <sabrinaperdiz.sp@gmail.com>
2022-12-20 20:18:55 -0300

    Update index.py

--- index.py
+++ index.py
@@ -1,7 +1,20 @@
 #import openFile as oF
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog, QPushButton, QLabel
+from PyQt5.QtWidgets import QApplication, QWidget, QLineEdit, QFileDialog, QPushButton, QLabel
 from PyQt5.QtGui import QIcon
+from pathlib import Path
+import pandas as pd
+
+#Função para selecionar o arquivo
+def AbreArquivo():
+    arquivo = QFileDialog.getOpenFileName(caption="Selecione o arquivo", filter= "CSV (*.csv)")[0]
+    with open (arquivo, 'r', encoding='utf8') as a:
+        dados = a.read() #str
+    dados = dados.replace("\n",";")
+    df = pd.read_csv(dados, sep=";")
+    
+        
+
 
 #Cria aplicação
 app = QApplication(sys.argv)
@@ -16,12 +29,17 @@ btn_abreArquivo = QPushButton("Abrir Arquivo",janela)
 #Define a posição e tamanho do botão
 btn_abreArquivo.setGeometry(300,300,100,30)
 
+#Evento click do botão
+btn_abreArquivo.clicked.connect(AbreArquivo)
+
 #Seta o CSS
 #btn_abreArquivo.setStyleSheet("")
 
+#Cria label
 label_titulo = QLabel("Analisador Foda", janela)
 label_titulo.move(250,100)
 label_titulo.setStyleSheet("font-size: 30px;")
+
 
 #Mostra a janela e executa a aplicação
 janela.show()
